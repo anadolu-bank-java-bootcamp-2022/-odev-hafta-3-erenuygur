36db8f30fb2e9d24642539fd8c03e5b2e95ff32c
Eren <ernuygur@gmail.com>
2022-06-24 19:20:23 +0300

    final commit

--- src/main/java/com/gokhantamkoc/javabootcamp/odevhafta3/service/ChartService.java
+++ src/main/java/com/gokhantamkoc/javabootcamp/odevhafta3/service/ChartService.java
@@ -1,42 +1,47 @@
-package com.gokhantamkoc.javabootcamp.odevhafta3.service;
+package com.gokhantamkoc.javabootcamp.odevhafta3.repository;
 
-import com.gokhantamkoc.javabootcamp.odevhafta3.repository.CSVRepository;
-import com.gokhantamkoc.javabootcamp.odevhafta3.util.chart.CandleStickChart;
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
 
-public class ChartService {
-	CSVRepository cryptoDataCSVRepository;
+import com.gokhantamkoc.javabootcamp.odevhafta3.model.Candle;
+
+public class CryptoDataCSVRepository implements CSVRepository {
 	
-	public ChartService(CSVRepository cryptoDataCSVRepository) {
-		this.cryptoDataCSVRepository = cryptoDataCSVRepository;
-	}
-	
-	public CandleStickChart createChartFromCryptoData() throws IOException {
-		// Bu metodu doldurmanizi bekliyoruz.
+	private final String COMMA_DELIMITER = ",";
 
-		CandleStickChart candleStickChart = new CandleStickChart("BTC/USDT");
+	@Override
+	public List<Candle> readCSV(String filename) throws FileNotFoundException, IOException {
+		List<Candle> candles = new ArrayList<Candle>();
+		InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream(filename);
+		// Bu alandan itibaren kodunuzu yazabilirsiniz
+		InputStreamReader  isr = new InputStreamReader(inputStream);
 
-		try {
-			List<Candle> candles = this.cryptoDataCSVRepository.readCSV("Binance_BTCUSDT_d.csv");
+		try (BufferedReader bufferedReader = new BufferedReader(isr)) {
+			String line = null;
 
-			for (Candle candle : candles) {
-				candleStickChart.addCandle(
-						candle.getTime(),
-						candle.getOpen(),
-						candle.getHigh(),
-						candle.getLow(),
-						candle.getClose(),
-						candle.getVolume());
+			int count = 0;
+			while ((line = bufferedReader.readLine()) != null) {
+
+				if (count != 0) {
+					String[] n = line.split(",");
+					Candle candle = new Candle(Long.parseLong(n[0]), Double.parseDouble(n[3]),
+							Double.parseDouble(n[4]), Double.parseDouble(n[5]), Double.parseDouble(n[6]),
+							Double.parseDouble(n[7]));
+
+					candles.add(candle);
+				}
+
+				++count;
 			}
 		}
-
 		catch (Exception ex) {
 			System.out.println(ex.getMessage());
 			ex.printStackTrace();
 		}
-		
-		return candleStickChart;
-	}
-}
 
+			// Bu alandan sonra kalan kod'a dokunmayiniz.
+		return candles;
+	}
+
+}
